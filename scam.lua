4178578ba0b5e501324b929f55d37b59da1830c0
Fruit <boskozarich@gmail.com>
2022-10-07 04:42:48 -0400

    Add files via upload

---
+++ scam.lua
@@ -0,0 +1,6 @@
+for _, perk in ipairs(perk_list) do
+    if (perk.id == "PERKS_LOTTERY") then
+        perk.name = "FUCKING SCAM"
+        perk.description = "DONT FALL FOR IT, ITS A SCAM"
+    end
+end
