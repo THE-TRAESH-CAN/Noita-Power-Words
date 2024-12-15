948ce0cf2ada92abbbe3c5870ae79c593d5b448a
Fruit <boskozarich@gmail.com>
2024-12-15 09:38:13 -0400

    Delete powerwords directory

--- powerwords/scam.lua
+++
@@ -1,6 +0,0 @@
-for _, perk in ipairs(perk_list) do
-    if (perk.id == "PERKS_LOTTERY") then
-        perk.name = "FUCKING SCAM"
-        perk.description = "DONT FALL FOR IT, ITS A SCAM"
-    end
-end
